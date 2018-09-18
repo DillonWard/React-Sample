0f37aa520d6cbbf83efd6695585475b725946bd4
Dillon Ward <Dillonward13@gmail.com>
2018-09-18 10:37:29 +0200

    added styling using className

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -6,14 +6,19 @@ export default class Counter extends Component {
         count: 0
     };
 
+    styles = {
+      fontSize: '10px',
+      fontWeight: 'bold'
+    };
+
   // wrap in a <div> because babel can only render 1 element at a time
   // wrapping it in a div can wrap multiple elements inside 1 - need a parent element
   // React.Fragment gets rid of unnecessary extra divs
   render() {
     return(
     <React.Fragment>
-      <span>{this.formatCounter()}</span>
-      <button>Increment</button>
+      <span style = { this.styles } className="badge badge-primary m-2">{this.formatCounter()}</span>
+      <button className="btn btn-secondary btn-sm">Increment</button>
     </React.Fragment>);
   }
 
