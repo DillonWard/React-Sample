580faf22e66b1ae94effa03409f312260212b033
Dillon Ward <Dillonward13@gmail.com>
2018-09-18 14:20:55 +0200

    implement state changes

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,41 +1,54 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
+  state = {
+    count: 0
+  };
 
-    state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
-    };
+  styles = {
+    fontSize: "10px",
+    fontWeight: "bold"
+  };
 
-    styles = {
-      fontSize: '10px',
-      fontWeight: 'bold'
-    };
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this)
+  // }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
 
   // wrap in a <div> because babel can only render 1 element at a time
   // wrapping it in a div can wrap multiple elements inside 1 - need a parent element
   // React.Fragment gets rid of unnecessary extra divs
   render() {
-
-    return(
-    <React.Fragment>
-      <span style = { this.styles } className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-      <button className="btn btn-secondary btn-sm">Increment</button>
-      <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>    
-    </React.Fragment>);
+    return (
+      <React.Fragment>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCounter()}
+        </span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     // append to the end of classes - if the counter is 0 append 'warning' otherwise append 'primary'
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCounter(){
-      const { count } = this.state;
+  formatCounter() {
+    const { count } = this.state;
 
-      // return count === 0 ? <h1>Zero</h1> : count;
-      return count === 0 ? 'Zero' : count;
+    // return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
